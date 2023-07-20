b2a90a369ef27590f12b18a32bc32d78120a59ea
shaehaggis <shae.haggis@gmail.com>
2023-07-20 15:42:50 +0930

    initial commit

---
+++ handleForm.js
@@ -0,0 +1,40 @@
+const bodyParser = require("body-parser")
+const express = require("express")
+const nodemailer = require("nodemailer")
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express()
+app.use(bodyParser.urlencoded())
+app.use(cors());
+
+const contactAddress = "shae.haggis@gmail.com"
+
+const mailer = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.GMAIL_ADDRESS,
+    pass: process.env.GMAIL_PASSWORD,
+  },
+})
+
+app.post("/contact", function (req, res) {
+
+  mailer.sendMail(
+    {
+      from: req.body.userEmail,
+      to: [contactAddress],
+      subject: req.body.subject || "[No subject]",
+      html: req.body.message || "[No message]",
+    },
+    function (err, info) {
+      if (err) {
+        console.log(err);
+        return res.status(500).send(err);
+      }
+      res.json({ success: true })
+    }
+  )
+})
+
+app.listen(3000)
